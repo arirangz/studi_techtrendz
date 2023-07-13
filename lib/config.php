119b4aec822ee51c6ae4e201931932a680348fb0
Anthony Laplane <anthony.laplane@gmail.com>
2023-07-13 19:01:11 +0200

    auth

--- lib/config.php
+++ lib/config.php
@@ -1,5 +1,6 @@
 <?php
 
+define("_DOMAIN_", ".techtrendzstudi.local");
 define("_DB_SERVER_", "localhost");
 define("_DB_NAME_", "studi_techtrendz_live");
 define("_DB_USER_", "root");
--- login.php
+++ login.php
@@ -1,19 +1,52 @@
-<?php 
-require_once __DIR__ . "/lib/menu.php"; 
-require_once __DIR__ . "/templates/header.php"; 
+<?php
+require_once __DIR__ . "/lib/config.php";
+require_once __DIR__ . "/lib/session.php";
+require_once __DIR__ . "/lib/pdo.php";
+require_once __DIR__ . "/lib/user.php";
+require_once __DIR__ . "/lib/menu.php";
+require_once __DIR__ . "/templates/header.php";
+
+$errors = [];
+
+if (isset($_POST["loginUser"])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $user = verifyUserLoginPassword($pdo, $email, $password);
+    if ($user) {
+        session_regenerate_id(true);
+        $_SESSION["user"] = $user;
+        if ($user["role"] === "user") {
+            header("location: index.php");
+        } elseif ($user["role"] === "admin") {
+            header("location: admin/index.php");
+        }
+
+    } else {
+        $errors[] = "Email ou mot de passe incorrect";
+    }
+}
+
+
 ?>
 
 
 <h1>Login</h1>
 
+<?php foreach ($errors as $error) { ?>
+    <div class="alert alert-danger">
+        <?=$error; ?>
+    </div>
+<?php } ?>
+
 <form method="post">
     <div class="mb-3">
         <label class="form-label" for="email">Email</label>
-        <input type="email" name="email" id="email" class="form-control">
+        <input type="email" name="email" id="email" class="form-control" required>
     </div>
     <div class="mb-3">
         <label class="form-label" for="password">Mot de passe</label>
-        <input type="password" name="password" id="password" class="form-control">
+        <input type="password" name="password" id="password" class="form-control" required>
     </div>
 
     <input type="submit" value="Connexion" name="loginUser" class="btn btn-primary">
